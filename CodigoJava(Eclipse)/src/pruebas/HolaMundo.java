a313b5dc108108f46918db8cfe60b16467ba7ab0
Castañeda <45492320+WorKeLid@users.noreply.github.com>
2019-09-23 14:38:33 +0200

    Create HolaMundo.java

---
+++ CodigoJava(Eclipse)/src/pruebas/HolaMundo.java
@@ -0,0 +1,31 @@
+package pruebas;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundo
+ */
+@WebServlet("/HolaMundo")
+public class HolaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h3>Hola mundo</h3>");
+		out.println("</body></html>");
+	}
+
+}
